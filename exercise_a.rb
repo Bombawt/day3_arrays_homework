bab1abe0002640324d6de02fae8e81454bf80507
Kira Marston <kiramarston@gmail.com>
2020-03-11 22:24:51 +0000

    Inserted Polmont between Falkirk High and Linlithgow

--- exercise_a.rb
+++ exercise_a.rb
@@ -3,5 +3,6 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High",
    "Linlithgow", "Livingston", "Haymarket" ]
    stops.push('Edinburgh Waverley')
    stops.unshift('Glasgow Queen St')
+   stops.insert(4,'Polmont')
 
    p stops
