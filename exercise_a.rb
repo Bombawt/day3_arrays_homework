d115911d42044c8fea359615ad136d74f306ce8c
Kira Marston <kiramarston@gmail.com>
2020-03-11 22:41:24 +0000

    Removed Livingston

--- exercise_a.rb
+++ exercise_a.rb
@@ -1,9 +1,12 @@
 
 stops = [ "Croy", "Cumbernauld", "Falkirk High",
    "Linlithgow", "Livingston", "Haymarket" ]
-   stops.push('Edinburgh Waverley')
-   stops.unshift('Glasgow Queen St')
-   stops.insert(4,'Polmont')
+# stops.push('Edinburgh Waverley')
+# stops.unshift('Glasgow Queen St')
+# stops.insert(4,'Polmont')
 
-   
-   p stops.index('Linlithgow')
+# p stops.index('Linlithgow')
+
+
+stops.delete("Livingston")
+p stops
